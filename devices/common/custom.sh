78d6d49552415a92d092bac25eb90645b487aeb7
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-06-14 05:16:56 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -51,7 +51,7 @@ cp -f devices/common/.config .config
 mv -f feeds/base feeds/base.bak
 mv -f feeds/packages feeds/packages.bak
 make defconfig
-rm -Rf tmp feeds/base
+rm -Rf feeds/base
 mv -f feeds/base.bak feeds/base
 mv -f feeds/packages.bak feeds/packages
 sed -i 's/CONFIG_ALL=y/CONFIG_ALL=n/' .config
