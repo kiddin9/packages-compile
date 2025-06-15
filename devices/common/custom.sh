5b97212753b2148e9a7f6ba2d39ecb682607cf4a
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-06-16 00:12:42 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -3,7 +3,7 @@
 shopt -s extglob
 rm -rf feeds/kiddin9/{diy,mt-drivers,shortcut-fe,luci-app-mtwifi,base-files,luci-app-package-manager,\
 dnsmasq,firewall*,wifi-scripts,opkg,ppp,curl,luci-app-firewall,\
-nftables,fstools,wireless-regdb,libnftnl}
+nftables,fstools,wireless-regdb,libnftnl,nginx-ui,netdata}
 rm -rf feeds/packages/libs/libcups
 
 curl -sfL https://raw.githubusercontent.com/openwrt/packages/master/lang/golang/golang/Makefile -o feeds/packages/lang/golang/golang/Makefile
