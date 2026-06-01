8b6014ec0022d24f21dee3cca00f0270505c0fa9
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2026-06-01 16:35:47 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -11,7 +11,7 @@ sed -i '/	refresh_config();/d' scripts/feeds
 ./scripts/feeds update -a
 
 rm -rf feeds/kiddin9/{diy,mt-drivers,shortcut-fe,luci-app-mtwifi,base-files,luci-app-package-manager,\
-dnsmasq,firewall*,wifi-scripts,opkg,ppp,curl,luci-app-firewall,\
+dnsmasq,firewall*,wifi-scripts,opkg,ppp,curl,\
 nftables,fstools,wireless-regdb,libnftnl}
 rm -rf feeds/packages/libs/libcups
 
